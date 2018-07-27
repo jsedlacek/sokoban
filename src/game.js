31b4d05e3d7a095ea06a6a8d0c5546c6fc33cf83
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2018-07-27 17:59:26 +0200

    add key handler to skeleton

--- src/game.js
+++ src/game.js
@@ -1,268 +1,296 @@
 import React from 'react';
 import './game.css';
 
-export default function Game() {
-  return (
-    <div className="Game">
-      <h1>SOKOBAN</h1>
-      <div className="Content">
-        <div className="Playground">
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+export default class Game extends React.Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell">
-              <div className="Player">👷</div>
-            </div>
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+  handleKeyDown = event => {
+    if (event.key === 'ArrowUp') {
+      console.log('Up');
+    }
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+    if (event.key === 'ArrowDown') {
+      console.log('Down');
+    }
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+    if (event.key === 'ArrowLeft') {
+      console.log('Left');
+    }
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
+    if (event.key === 'ArrowRight') {
+      console.log('Right');
+    }
+  };
 
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Target" />
-              <div className="Box Box-target" />
-            </div>
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell">
-              <div className="Box" />
-            </div>
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
-
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Target" />
-            </div>
-            <div className="Cell" />
-            <div className="Cell" />
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
-
-          <div className="Row">
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-            <div className="Cell">
-              <div className="Wall" />
-            </div>
-          </div>
-        </div>
-
-        <div className="Stats">
-          <div>Time: 00:00</div>
-          <div>Turns: 0</div>
-
-          <div className="Legend">
-            <div className="Legend-item">
-              <div className="Legend-symbol">
-                <div className="Cell">
-                  <div className="Player">👷</div>
-                </div>
+  render() {
+    return (
+      <div className="Game">
+        <h1>SOKOBAN</h1>
+        <div className="Content">
+          <div className="Playground">
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
               </div>
-              <div className="Legend-label">Player</div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
             </div>
 
-            <div className="Legend-item">
-              <div className="Legend-symbol">
-                <div className="Cell">
-                  <div className="Wall" />
-                </div>
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
               </div>
-              <div className="Legend-label">Wall</div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
             </div>
 
-            <div className="Legend-item">
-              <div className="Legend-symbol">
-                <div className="Cell">
-                  <div className="Box" />
-                </div>
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
               </div>
-              <div className="Legend-label">Box</div>
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell">
+                <div className="Player">👷</div>
+              </div>
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
             </div>
 
-            <div className="Legend-item">
-              <div className="Legend-symbol">
-                <div className="Cell">
-                  <div className="Target" />
-                </div>
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
               </div>
-              <div className="Legend-label">Target</div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Target" />
+                <div className="Box Box-target" />
+              </div>
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell">
+                <div className="Box" />
+              </div>
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Target" />
+              </div>
+              <div className="Cell" />
+              <div className="Cell" />
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+
+            <div className="Row">
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+              <div className="Cell">
+                <div className="Wall" />
+              </div>
+            </div>
+          </div>
+
+          <div className="Stats">
+            <div>Time: 00:00</div>
+            <div>Turns: 0</div>
+
+            <div className="Legend">
+              <div className="Legend-item">
+                <div className="Legend-symbol">
+                  <div className="Cell">
+                    <div className="Player">👷</div>
+                  </div>
+                </div>
+                <div className="Legend-label">Player</div>
+              </div>
+
+              <div className="Legend-item">
+                <div className="Legend-symbol">
+                  <div className="Cell">
+                    <div className="Wall" />
+                  </div>
+                </div>
+                <div className="Legend-label">Wall</div>
+              </div>
+
+              <div className="Legend-item">
+                <div className="Legend-symbol">
+                  <div className="Cell">
+                    <div className="Box" />
+                  </div>
+                </div>
+                <div className="Legend-label">Box</div>
+              </div>
+
+              <div className="Legend-item">
+                <div className="Legend-symbol">
+                  <div className="Cell">
+                    <div className="Target" />
+                  </div>
+                </div>
+                <div className="Legend-label">Target</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
